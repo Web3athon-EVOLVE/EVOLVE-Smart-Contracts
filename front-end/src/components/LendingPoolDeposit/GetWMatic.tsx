f90647d9c923f38d3b1506a0748eb961819dfd0e
Daiana Bilbao <dbilbao@cs.colostate.edu>
2022-09-19 16:52:36 -0600

    deposited funds into pool UI

--- front-end/src/components/LendingPoolDeposit/GetWMatic.tsx
+++ front-end/src/components/LendingPoolDeposit/GetWMatic.tsx
@@ -2,28 +2,27 @@ import React, { useState, useEffect } from 'react'
 import { Form, ButtonGroup, Button, InputGroup } from 'react-bootstrap';
 import { useContract, useSigner } from 'wagmi'
 import { ethers } from "ethers";
-import Alert from "../Alert";
+import Alert from "../Utils/Alert";
 
 import { tokenAddresses } from "../../../../smart-contracts/utils/tokenAddresses";
 
 import IWMatic_JSON from "../../../../smart-contracts/artifacts/contracts/interfaces/IWMatic.sol/IWMatic.json";
-import IERC_JSON from "../../../../smart-contracts/artifacts/contracts/interfaces/IERC20.sol/IERC20.json";
 
 export default function GetWMatic(matic) {
-    // State
+    // component states
     const [mounted, setMounted] = useState(false);
     const [amount, setQuantity] = useState('');
     const [validAmount, setValidAmount] = useState(false);
-    // conts
-    const WMATIC_TOKEN = tokenAddresses["Polygon Mumbai"]["ERC20"]["wMATIC"];
-    // transaction state
+    // transaction states
     const [success, setSuccess] = useState(null);
     const [message, setMessage] = useState(null);
     const [loading, setLoading] = useState(null);
+    // constants
+    const WMATIC_TOKEN = tokenAddresses["Polygon Mumbai"]["ERC20"]["wMATIC"];
 
     useEffect(() => setMounted(true), []);
 
-    // Mumabi Contract Setup
+    // WMATIC Contract Setup
     const signer = useSigner();
     const IWMatic_Contract = useContract({
         addressOrName: WMATIC_TOKEN,
@@ -31,12 +30,7 @@ export default function GetWMatic(matic) {
         signerOrProvider: signer.data,
     });
 
-    const IERC20_Contract = useContract({
-        addressOrName: WMATIC_TOKEN,
-        contractInterface: IERC_JSON.abi,
-        signerOrProvider: signer.data,
-    });
-
+    // Component Functions
     function setAmount (perc) {
         const amount = Math.floor(Number(matic.balance) * perc) / 100;
         let isValid = !isNaN(amount) && amount > 0;
@@ -58,20 +52,17 @@ export default function GetWMatic(matic) {
             console.log("Invalid token amount");
             return
         }
-
         try {
             if(IWMatic_Contract) {
                 console.log("Getting wMATIC for by depositing 0.01 MATIC: ");
-                let IWMatic_TXN = await IWMatic_Contract.deposit({"value": ethers.utils.parseUnits("0.01")});
-
+                let IWMatic_TXN = await IWMatic_Contract.deposit({"value": ethers.utils.parseUnits(amount)});
                 setLoading(true);
                 console.log("Minting...", IWMatic_TXN.hash);
                 let IWMatic_WAIT = await IWMatic_TXN.wait();
                 console.log("Minted -- ", IWMatic_TXN.hash);
-        
                 setSuccess(true);
                 setLoading(false);
-                setMessage("Successfully exchange MATIC for wMATIC!");
+                setMessage("Successfully exchanged MATIC for wMATIC! Refresh Page To See Update or Check Your Wallet.");
             }
             else {
                 console.log("Error getting contract.");
@@ -88,30 +79,6 @@ export default function GetWMatic(matic) {
     if (!mounted) return null
     return (
         <div>
-        {loading && (
-          <Alert
-            alertType={"loading"}
-            alertBody={"Please wait"}
-            triggerAlert={true}
-            color={"white"}
-          />
-        )}
-        {success && (
-          <Alert
-            alertType={"success"}
-            alertBody={message}
-            triggerAlert={true}
-            color={"palegreen"}
-          />
-        )}
-        {success === false && (
-          <Alert
-            alertType={"failed"}
-            alertBody={message}
-            triggerAlert={true}
-            color={"palevioletred"}
-          />
-        )}
         <p>Exchange Your MATIC for wMATIC</p>
         <Form className="p-4">
             {/* Input Deposit Amount */}
@@ -142,5 +109,29 @@ export default function GetWMatic(matic) {
                 className="w-40 m-4 bg-indigo-500 hover:bg-indigo-700 text-white font-bold rounded"> Get wMATIC
             </Button>
         </Form>
+        {loading && (
+          <Alert
+            alertType={"loading"}
+            alertBody={"Please wait..."}
+            triggerAlert={true}
+            color={"white"}
+          />
+        )}
+        {success && (
+          <Alert
+            alertType={"success"}
+            alertBody={message}
+            triggerAlert={true}
+            color={"palegreen"}
+          />
+        )}
+        {success === false && (
+          <Alert
+            alertType={"failed"}
+            alertBody={message}
+            triggerAlert={true}
+            color={"palevioletred"}
+          />
+        )}
     </div>);
 }
--- front-end/src/components/LendingPoolDeposit/StakeForm.tsx
+++ front-end/src/components/LendingPoolDeposit/StakeForm.tsx
@@ -1,18 +1,34 @@
 import React, { useState, useEffect } from 'react'
+
 import { Form, ButtonGroup, Button, InputGroup } from 'react-bootstrap';
+import Alert from "../Utils/Alert";
+
 import { useContract, useSigner } from 'wagmi'
+import { ethers } from "ethers";
+
 import { mumbaiContractAddress } from "../../../../smart-contracts/utils/contractAddress";
+import { tokenAddresses } from "../../../../smart-contracts/utils/tokenAddresses";
+
 import abiJSON from "../../../../smart-contracts/artifacts/contracts/CommunityPool.sol/CommunityPool.json";
+import IERC_JSON from "../../../../smart-contracts/artifacts/contracts/interfaces/IERC20.sol/IERC20.json";
 
 export default function StakeForm(wMatic) {
+    // component states
     const [mounted, setMounted] = useState(false);
     const [amount, setQuantity] = useState('');
     const [validAmount, setValidAmount] = useState(false);
     const [formType, setForm] = useState('stake');
+    // transaction states
+    const [success, setSuccess] = useState(null);
+    const [message, setMessage] = useState(null);
+    const [loading, setLoading] = useState(null);
+    // constants
     const title = (formType === 'stake') ? "Stake" : (formType === 'unstake') ? "Unstake" : undefined;
+    const WMATIC_TOKEN = tokenAddresses["Polygon Mumbai"]["ERC20"]["wMATIC"];
+
     useEffect(() => setMounted(true), []);
 
-    // Mumabi Contract Setup
+    // Lending Pool Contract Setup
     const signer = useSigner();
     const contractOnMumbai = useContract({
         addressOrName: mumbaiContractAddress,
@@ -20,6 +36,14 @@ export default function StakeForm(wMatic) {
         signerOrProvider: signer.data,
     });
 
+    // IERC20 Contract Setup
+    const IERC20_Contract = useContract({
+        addressOrName: WMATIC_TOKEN,
+        contractInterface: IERC_JSON.abi,
+        signerOrProvider: signer.data,
+    });
+
+    // Form Functions
     function setAmount (perc) {
         const amount = Math.floor(Number(wMatic.balance) * perc) / 100;
         let isValid = !isNaN(amount) && amount > 0;
@@ -36,7 +60,7 @@ export default function StakeForm(wMatic) {
         setQuantity(e.target.value);
     }
 
-    function submitForm () {
+    async function submitForm () {
         if (formType === 'stake') {
             submitStake()
         } else if (formType === 'unstake') {
@@ -44,17 +68,59 @@ export default function StakeForm(wMatic) {
         }
     }
 
-    function submitStake() {
+    async function submitStake() {
         if (!validAmount) {
             console.log("Invalid token amount");
             return
         }
-        setForm('unstake');
-        console.log("TODO: STAKE");
-        console.log("AMOUNT: ", amount);
+        try {
+            if(IERC20_Contract) {
+                console.log("Approve CommunityPool Contract to spend Caller's Tokens: ");
+                let IERC20_TXN = await IERC20_Contract.approve(mumbaiContractAddress,  ethers.utils.parseUnits(amount));
+                setLoading(true);
+                console.log("Minting...", IERC20_TXN.hash);
+                let IERC20_WAIT = await IERC20_TXN.wait();
+                console.log("Minted -- ", IERC20_TXN.hash);
+                setSuccess(true);
+                setLoading(false);
+                setMessage("Successfully gave permission to EVOLVE to deposit your funds and earn rewards...");
+            }
+            else {
+                console.log("Error getting contract.");
+            }
+        }
+        catch (error) {
+            setSuccess(false);
+            setMessage(`There was an error in giving permission to EVOLVE to deposit your funds: ${error.message}`);
+            setLoading(false);
+            console.log(error);
+        }
+        try {
+            if(mumbaiContractAddress) {
+                console.log("Depositing into Community Pool: ");
+                let contractOnMumbai_TXN = await contractOnMumbai.poolDeposit(WMATIC_TOKEN, ethers.utils.parseUnits(amount), { gasLimit: 2074040 });
+                setLoading(true);
+                console.log("Minting...", contractOnMumbai_TXN.hash);
+                let contractOnMumbai_WAIT = await contractOnMumbai_TXN.wait();
+                console.log("Minted -- ", contractOnMumbai_TXN.hash);
+                setSuccess(true);
+                setLoading(false);
+                setForm('unstake');
+                setMessage("Congratulations! Your funds were successfully deposited, you're on your way to financial freedom!");
+            }
+            else {
+                console.log("Error getting contract.");
+            }
+        }
+        catch (error) {
+            setSuccess(false);
+            setMessage(`There was an error during depositing your funds into our EVOLVE Pool: ${error.message}`);
+            setLoading(false);
+            console.log(error);
+        }
     }
 
-    function submitUnstake() {
+    async function submitUnstake() {
         if (!validAmount) {
             console.log("Invalid token amount");
             return
@@ -97,5 +163,29 @@ export default function StakeForm(wMatic) {
                 className="w-40 m-4 bg-indigo-500 hover:bg-indigo-700 text-white font-bold rounded">{title}
             </Button>
         </Form>
+        {loading && (
+          <Alert
+            alertType={"loading"}
+            alertBody={"Please wait..."}
+            triggerAlert={true}
+            color={"white"}
+          />
+        )}
+        {success && (
+          <Alert
+            alertType={"success"}
+            alertBody={message}
+            triggerAlert={true}
+            color={"palegreen"}
+          />
+        )}
+        {success === false && (
+          <Alert
+            alertType={"failed"}
+            alertBody={message}
+            triggerAlert={true}
+            color={"palevioletred"}
+          />
+        )}
     </div>);
 }
