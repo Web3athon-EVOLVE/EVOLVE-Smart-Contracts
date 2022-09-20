2bf218f62dd68e9604b62575f563f5f699818967
Daiana Bilbao <dbilbao@cs.colostate.edu>
2022-09-19 19:01:15 -0600

    updating desc

--- front-end/src/appConfig.ts
+++ front-end/src/appConfig.ts
@@ -1,9 +1,9 @@
 export const app: { [key: string]: string } = {
   name: 'Evolution Pool', // web app name
   title: 'Evolution Pool', // website title
-  description: 'dApp Starter Boilerplate by https://github.com/arisac', // description of website
+  description: 'Evolution Pool DApp for Community Lending & Borrowing through AAVE', // description of website
   keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
-  url: 'https://dapp-starter.aris.ac', // website url
+  url: '', // website url
   image: '/icon-512x512.png', // image url for SEO
   favicon: '/favicon.ico', // favicon url
   twitter: '@arisdotac', // twitter account
